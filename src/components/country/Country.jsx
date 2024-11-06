c324ff275857e3d0ce49970e13a597ea4354bb89
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-11-06 08:31:38 +0600

    addded a world country Tou

---
+++ src/components/country/Country.jsx
@@ -0,0 +1,17 @@
+import './Country.css'
+
+const Country = ({country}) => {
+   const {name, flags, population, area, capital} = country
+    return (
+        <div className='country'>
+            <h3>{name.common}</h3>
+           <img src={flags.png} alt="" />
+           <p>Population: {population}</p>
+           <p>Capital: {capital}</p>
+           
+           <p>Area: {area}</p>
+        </div>
+    );
+};
+
+export default Country;
